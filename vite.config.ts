5bdfce28825834fcbac4e03db386220f7411f162
Pavel <pavelmalyv@gmail.com>
2025-02-25 18:58:15 +0300

    init: выполнить базовую настройку vite

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+	resolve: {
+		alias: {
+			'@': '/src',
+			'@styles': '/src/styles',
+		},
+	},
+	plugins: [
+		react(),
+		checker({
+			typescript: true,
+			eslint: {
+				lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+				useFlatConfig: true,
+			},
+			stylelint: {
+				lintCommand: 'stylelint ./src/**/*.{css,scss}',
+			},
+		}),
+	],
+});
