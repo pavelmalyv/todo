4c401ebd542f868cf9176c347b0b6a5cd2f80166
Pavel <pavelmalyv@gmail.com>
2025-02-26 17:11:05 +0300

    feat(Authorization): добавить стили

--- src/components/authorization/Authorization.tsx
+++ src/components/authorization/Authorization.tsx
@@ -2,6 +2,7 @@ import cl from './Authorization.module.scss';
 import classNames from 'classnames';
 import SmallForm from '@/components/Forms/smallForm/SmallForm';
 import Field from '../UI/field/Field';
+import LoginSocials from '../loginSocials/LoginSocials';
 
 import { Link } from 'react-router';
 
@@ -12,14 +13,22 @@ interface AuthorizationProps {
 const Authorization = ({ titleId }: AuthorizationProps) => {
 	return (
 		<div className={cl.authorization}>
-			<div className={cl.registration}>
-				<h1 id={titleId} className={classNames('h1', cl.title)}>
-					Вход
-				</h1>
+			<h1 id={titleId} className={classNames('h1', cl.title)}>
+				Вход
+			</h1>
+
+			<div className={cl.form}>
 				<SmallForm aria-labelledby={titleId} buttonName="Войти">
 					<Field label="Email" placeholder="Email" />
 					<Field label="Пароль" placeholder="Пароль" isProtected={true} />
 				</SmallForm>
+			</div>
+
+			<div className={cl['login-socials']}>
+				<LoginSocials />
+			</div>
+
+			<div className={cl.registration}>
 				Нет аккаунта?{' '}
 				<Link to={'#'} className="link">
 					Зарегистрироваться
