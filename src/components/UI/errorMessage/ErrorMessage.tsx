0d925275f658f4121519e4488e94656577f602b1
Pavel <pavelmalyv@gmail.com>
2025-03-07 18:09:43 +0300

    feat(ErrorMessage): добавить вывод ошибки в консоль

--- src/components/UI/errorMessage/ErrorMessage.tsx
+++ src/components/UI/errorMessage/ErrorMessage.tsx
@@ -1,12 +1,18 @@
 import classNames from 'classnames';
 import cl from './ErrorMessage.module.scss';
+import { useEffect } from 'react';
 
 interface ErrorMessageProps {
 	message: string | undefined;
+	error?: unknown;
 	className?: string;
 }
 
-const ErrorMessage = ({ message, className }: ErrorMessageProps) => {
+const ErrorMessage = ({ message, error, className }: ErrorMessageProps) => {
+	useEffect(() => {
+		console.error(error);
+	}, [error]);
+
 	return (
 		<div className={classNames(cl.error, className)} role="status">
 			{message}
