11ddd0f60ee30bcb68f5deabe1ae67998724cf32
Pavel <pavelmalyv@gmail.com>
2025-03-13 22:26:09 +0300

    feat(Section): добавить пропс titleId

--- src/components/UI/section/Section.tsx
+++ src/components/UI/section/Section.tsx
@@ -5,16 +5,18 @@ import { useId } from 'react';
 
 interface SectionProps {
 	title: string;
+	titleId?: string;
 	children: React.ReactNode;
 }
 
-const Section = ({ title, children }: SectionProps) => {
-	const titleId = useId();
+const Section = ({ title, titleId, children }: SectionProps) => {
+	const titleIdGenerated = useId();
+	const titleIdSection = titleId ? titleId : titleIdGenerated;
 
 	return (
-		<section className="section" aria-labelledby={titleId}>
+		<section className="section" aria-labelledby={titleIdSection}>
 			<div className={cl.body}>
-				<h2 id={titleId} className={classNames(cl.title, 'h1')}>
+				<h2 id={titleIdSection} className={classNames(cl.title, 'h1')}>
 					{title}
 				</h2>
 
