5035f6be40acb1f39b4ac8f69d89856a05f54d72
Pavel <pavelmalyv@gmail.com>
2025-03-18 17:53:13 +0300

    feat(TodayPage): добавить пагинацию

--- src/pages/TodayPage.tsx
+++ src/pages/TodayPage.tsx
@@ -1,43 +1,71 @@
 import Profile from '@/components/profile/Profile';
 import Section from '@/components/UI/section/Section';
 import TasksList from '@/components/tasksList/TasksList';
+import Button from '@/components/UI/button/Button';
 import useTasksSnapshot from '@/hooks/useTasksSnapshot';
 import useQuantityTasksSnapshot from '@/hooks/useQuantityTasksSnapshot';
 import useNotificationError from '@/hooks/useNotificationError';
 
 import { getDateRanges } from '@/utils/date';
+import { getQuantityShort } from '@/utils/quantity';
+import { showError } from '@/utils/notification';
+import { useId } from 'react';
 import { LIMIT_QUANTITY_TODAY, LIMIT_TODAY } from '@/consts/docLimits';
 import { ERRORS_MESSAGES, NOT_FOUND_MESSAGES } from '@/consts/messages';
-import { getQuantityShort } from '@/utils/quantity';
 
 const TodayPage = () => {
 	const dateRanges = getDateRanges();
 
-	const [tasks, isLoading, errorToday] = useTasksSnapshot({
-		timestampStart: dateRanges.today.start,
-		timestampEnd: dateRanges.today.end,
-		limit: LIMIT_TODAY,
-	});
-	const tasksToday = tasks ? tasks : new Array(10).fill(null);
+	const [tasks, isLoading, errorToday, { fetchMore, isLoadingMore, hasMoreData }] =
+		useTasksSnapshot({
+			timestampStart: dateRanges.today.start,
+			timestampEnd: dateRanges.today.end,
+			limit: LIMIT_TODAY,
+		});
+	const tasksToday = tasks ? tasks : new Array(LIMIT_TODAY).fill(null);
 
 	const [quantity, isLoadingQuantity, errorQuantityToday] = useQuantityTasksSnapshot({
 		timestampStart: dateRanges.today.start,
 		timestampEnd: dateRanges.today.end,
 		limit: LIMIT_QUANTITY_TODAY + 1,
 	});
+
 	useNotificationError(ERRORS_MESSAGES.quantityTodayTasksLoading, errorQuantityToday);
 
+	const handleFetchMore = () => {
+		try {
+			fetchMore();
+		} catch (error) {
+			showError(ERRORS_MESSAGES.tasksLoading, error);
+		}
+	};
+
 	const quantityShort = quantity !== null ? getQuantityShort(quantity, LIMIT_QUANTITY_TODAY) : null;
+	const titleId = useId();
 
 	return (
 		<Profile title="Сегодня" quantity={quantityShort} isLoadingQuantity={isLoadingQuantity}>
-			<Section title="Задачи">
+			<Section title="Задачи" titleId={titleId}>
 				<TasksList
 					tasks={tasksToday}
 					isLoading={isLoading}
 					notFoundMessage={NOT_FOUND_MESSAGES.todayTasks}
 					error={errorToday}
-				/>
+					aria-labelledby={titleId}
+				>
+					{hasMoreData && (
+						<TasksList.Button>
+							<Button
+								style="border"
+								size="small"
+								onClick={handleFetchMore}
+								isLoading={isLoadingMore}
+							>
+								Загрузить еще
+							</Button>
+						</TasksList.Button>
+					)}
+				</TasksList>
 			</Section>
 		</Profile>
 	);
