f56646f12d13fc5a09721a9e8e934a6091f784fe
Pavel <pavelmalyv@gmail.com>
2025-03-18 19:03:11 +0300

    refactor(docLimits): переименовать константу

--- src/consts/docLimits.ts
+++ src/consts/docLimits.ts
@@ -1,3 +1,3 @@
 export const LIMIT_UPCOMING_TASKS = 4;
 export const LIMIT_QUANTITY_TASKS = 10;
-export const LIMIT_DAY_TASKS = 10;
+export const LIMIT_DAILY_TASKS = 10;
--- src/pages/TomorrowPage.tsx
+++ src/pages/TomorrowPage.tsx
@@ -1,7 +1,7 @@
 import DailyTasks from '@/components/dailyTasks/DailyTasks';
 
 import { getDateRanges } from '@/utils/date';
-import { LIMIT_QUANTITY_TASKS, LIMIT_DAY_TASKS } from '@/consts/docLimits';
+import { LIMIT_QUANTITY_TASKS, LIMIT_DAILY_TASKS } from '@/consts/docLimits';
 import { ERRORS_MESSAGES, NOT_FOUND_MESSAGES } from '@/consts/messages';
 
 const TomorrowPage = () => {
@@ -13,7 +13,7 @@ const TomorrowPage = () => {
 			subtitle="Задачи"
 			timestampStart={dateRanges.tomorrow.start}
 			timestampEnd={dateRanges.tomorrow.end}
-			limit={LIMIT_DAY_TASKS}
+			limit={LIMIT_DAILY_TASKS}
 			limitQuantity={LIMIT_QUANTITY_TASKS}
 			errorMessageQuantityLoading={ERRORS_MESSAGES.quantityTomorrowTasksLoading}
 			errorMessageTasksLoading={ERRORS_MESSAGES.tasksLoading}
