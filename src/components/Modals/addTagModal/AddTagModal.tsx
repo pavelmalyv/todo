6e1c06db530ce379af42a6be00339264ba695562
Pavel <pavelmalyv@gmail.com>
2025-03-19 19:19:42 +0300

    feat(AddTagModal): добавить компонент AddTagModal

---
+++ src/components/Modals/addTagModal/AddTagModal.tsx
@@ -0,0 +1,90 @@
+import SmallForm from '@/components/Forms/smallForm/SmallForm';
+import AppModal from '@/components/UI/appModal/AppModal';
+import Field from '@/components/UI/field/Field';
+import FieldColor from '@/components/UI/fieldColor/FieldColor';
+import useAddTag from '@/hooks/useAddTag';
+
+import { hexColorSchema, nameTagSchema } from '@/schemas/fields';
+import { showError, showSuccess } from '@/utils/notification';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useId } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { InferType, object } from 'yup';
+import { ERRORS_MESSAGES, SUCCESS_MESSAGES } from '@/consts/messages';
+
+interface AddTagModalProps {
+	isOpen: boolean;
+	onClose: () => void;
+}
+
+const addTagFormSchema = object({
+	name: nameTagSchema,
+	color: hexColorSchema,
+});
+
+type AddTagFormData = InferType<typeof addTagFormSchema>;
+
+const AddTagModal = ({ isOpen, onClose }: AddTagModalProps) => {
+	const [addTag, isLoading] = useAddTag();
+	const titleId = useId();
+
+	const { control, handleSubmit, reset } = useForm<AddTagFormData>({
+		resolver: yupResolver(addTagFormSchema),
+		defaultValues: {
+			name: '',
+			color: '#76DE37',
+		},
+	});
+
+	const onSubmit: SubmitHandler<AddTagFormData> = async (data) => {
+		try {
+			await addTag(data);
+
+			reset();
+			onClose();
+			showSuccess(SUCCESS_MESSAGES.addTag);
+		} catch (error) {
+			showError(ERRORS_MESSAGES.addTag, error);
+		}
+	};
+
+	return (
+		<AppModal isOpen={isOpen} onClose={onClose} aria-labelledby={titleId}>
+			<AppModal.Title id={titleId}>Добавить новый тег</AppModal.Title>
+			<SmallForm isLoading={isLoading} buttonName="Добавить" onSubmit={handleSubmit(onSubmit)}>
+				<Controller
+					name="name"
+					control={control}
+					render={({ field, fieldState }) => (
+						<Field
+							label="Название тега"
+							placeholder="Название тега"
+							aria-required={true}
+							aria-invalid={fieldState.invalid}
+							errorMessage={fieldState.error?.message}
+							{...field}
+						/>
+					)}
+				/>
+				<Controller
+					name="color"
+					control={control}
+					render={({ field, fieldState }) => (
+						<FieldColor
+							label="Цвет:"
+							ref={field.ref}
+							name={field.name}
+							value={field.value}
+							onChange={field.onChange}
+							aria-required={true}
+							aria-invalid={fieldState.invalid}
+							errorMessage={fieldState.error?.message}
+						/>
+					)}
+				/>
+			</SmallForm>
+		</AppModal>
+	);
+};
+
+export default AddTagModal;
