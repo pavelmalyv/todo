1875b627b915c4a2907a6dca3bd0dc8e2a9e8504
Pavel <pavelmalyv@gmail.com>
2025-03-19 19:20:10 +0300

    feat(TagsList): интегрировать компонент AddTagModal

--- src/components/tagsList/TagsList.tsx
+++ src/components/tagsList/TagsList.tsx
@@ -1,45 +1,58 @@
 import cl from './TagsList.module.scss';
 import TagMarker from '../UI/tagMarker/TagMarker';
 import ButtonIconText from '../UI/buttonIconText/ButtonIconText';
+import AddTagModal from '../Modals/addTagModal/AddTagModal';
 
 import { NavLink } from 'react-router';
+import { useState } from 'react';
 
 const TagsList = () => {
+	const [isOpenAddTag, setIsOpenAddTag] = useState(false);
+
 	return (
-		<div className={cl.tags}>
-			<nav>
-				<ul className={cl.list}>
-					<li className={cl.item}>
-						<NavLink className={cl.link} to={'/'}>
-							<TagMarker color="#FF0000" />
+		<>
+			<div className={cl.tags}>
+				<nav>
+					<ul className={cl.list}>
+						<li className={cl.item}>
+							<NavLink className={cl.link} to={'/'}>
+								<TagMarker color="#FF0000" />
 
-							<span className={cl.name}>Работа</span>
-						</NavLink>
-					</li>
-					<li className={cl.item}>
-						<NavLink className={cl.link} to={'other/'}>
-							<TagMarker color="#FF0000" />
+								<span className={cl.name}>Работа</span>
+							</NavLink>
+						</li>
+						<li className={cl.item}>
+							<NavLink className={cl.link} to={'other/'}>
+								<TagMarker color="#FF0000" />
 
-							<span className={cl.name}>Личное</span>
-						</NavLink>
-					</li>
-					<li className={cl.item}>
-						<NavLink className={cl.link} to={'other/'}>
-							<TagMarker color="#FF0000" />
+								<span className={cl.name}>Личное</span>
+							</NavLink>
+						</li>
+						<li className={cl.item}>
+							<NavLink className={cl.link} to={'other/'}>
+								<TagMarker color="#FF0000" />
 
-							<span className={cl.name}>
-								В частности, разбавленное изрядной долей эмпатии, рациональное мышление не оставляет
-								шанса для вывода текущих активов
-							</span>
-						</NavLink>
-					</li>
-				</ul>
-			</nav>
+								<span className={cl.name}>
+									В частности, разбавленное изрядной долей эмпатии, рациональное мышление не
+									оставляет шанса для вывода текущих активов
+								</span>
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
 
-			<ButtonIconText className={cl['button']} icon="add_circle" size="small">
-				Добавить тег
-			</ButtonIconText>
-		</div>
+				<ButtonIconText
+					className={cl['button']}
+					icon="add_circle"
+					size="small"
+					onClick={() => setIsOpenAddTag(true)}
+				>
+					Добавить тег
+				</ButtonIconText>
+			</div>
+
+			<AddTagModal isOpen={isOpenAddTag} onClose={() => setIsOpenAddTag(false)} />
+		</>
 	);
 };
 
