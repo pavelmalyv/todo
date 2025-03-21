c35f4ebbd4b7c8fee1298050be9d0b9c9ecc45da
Pavel <pavelmalyv@gmail.com>
2025-03-21 15:59:49 +0300

    feat(FieldColor): добавить скелетон

--- src/components/UI/fieldColor/FieldColor.tsx
+++ src/components/UI/fieldColor/FieldColor.tsx
@@ -1,6 +1,8 @@
 import cl from './FieldColor.module.scss';
 import classNames from 'classnames';
 import FieldError from '../fieldError/FieldError';
+import VisuallyHiddenLoader from '@/components/visuallyHiddenLoader/VisuallyHiddenLoader';
+import Skeleton from 'react-loading-skeleton';
 
 import { SketchPicker } from 'react-color';
 import { forwardRef, useId } from 'react';
@@ -10,6 +12,8 @@ interface FieldColorProps {
 	name?: string;
 	value: string;
 	errorMessage?: string;
+	isLoading?: boolean;
+	isSkeleton?: boolean;
 	onChange: (color: string) => void;
 	'aria-invalid'?: boolean;
 	'aria-required'?: boolean;
@@ -22,6 +26,8 @@ const FieldColor = forwardRef<HTMLInputElement, FieldColorProps>(
 			name,
 			value,
 			errorMessage,
+			isLoading = false,
+			isSkeleton = false,
 			onChange,
 			'aria-invalid': ariaInvalid,
 			'aria-required': ariaRequired,
@@ -35,63 +41,76 @@ const FieldColor = forwardRef<HTMLInputElement, FieldColorProps>(
 		return (
 			<div>
 				<div className={cl['head-wrapper']}>
-					<div className={cl.head}>
-						<label htmlFor={titleId} className={cl.label}>
-							{label}
-						</label>
-						<input
-							ref={ref}
-							id={titleId}
-							type="text"
-							className={classNames('visually-hidden', cl.field)}
-							name={name}
-							value={value}
-							readOnly={true}
-							autoComplete="none"
-							aria-describedby={errorMessageId}
-							aria-invalid={ariaInvalid}
-							aria-required={ariaRequired}
-							aria-controls={pickerId}
-						/>
-						<div
-							className={cl.selected}
-							style={{ backgroundColor: `${value}` }}
-							aria-hidden={true}
-						></div>
-					</div>
-
-					<FieldError id={errorMessageId} message={errorMessage ? errorMessage : null} />
+					<VisuallyHiddenLoader isLoading={isLoading}>
+						{isSkeleton ? (
+							<Skeleton className={cl['skeleton-field']} />
+						) : (
+							<>
+								<div className={cl.head}>
+									<label htmlFor={titleId} className={cl.label}>
+										{label}
+									</label>
+									<input
+										ref={ref}
+										id={titleId}
+										type="text"
+										className={classNames('visually-hidden', cl.field)}
+										name={name}
+										value={value}
+										readOnly={true}
+										autoComplete="none"
+										aria-describedby={errorMessageId}
+										aria-invalid={ariaInvalid}
+										aria-required={ariaRequired}
+										aria-controls={pickerId}
+									/>
+									<div
+										className={cl.selected}
+										style={{ backgroundColor: `${value}` }}
+										aria-hidden={true}
+									></div>
+								</div>
+								<FieldError id={errorMessageId} message={errorMessage ? errorMessage : null} />{' '}
+							</>
+						)}
+					</VisuallyHiddenLoader>
 				</div>
 
 				<div id={pickerId} aria-label="Выбор цвета">
-					<SketchPicker
-						color={value}
-						disableAlpha={true}
-						onChange={(color) => onChange(color.hex)}
-						presetColors={[
-							'#76DE37',
-							'#417505',
-							'#D0021B',
-							'#F5A623',
-							'#F8E71C',
-							'#8B572A',
-							'#BD10E0',
-							'#9013FE',
-							'#4A90E2',
-							'#50E3C2',
-							'#9B9B9B',
-						]}
-						styles={{
-							default: {
-								color: {
-									display: 'none',
-								},
-								saturation: {
-									paddingBottom: '40%',
-								},
-							},
-						}}
-					/>
+					<VisuallyHiddenLoader isLoading={isLoading}>
+						{isSkeleton ? (
+							<Skeleton className={cl['skeleton-picker']} />
+						) : (
+							<SketchPicker
+								color={value}
+								disableAlpha={true}
+								onChange={(color) => onChange(color.hex)}
+								presetColors={[
+									'#76DE37',
+									'#417505',
+									'#D0021B',
+									'#F5A623',
+									'#F8E71C',
+									'#8B572A',
+									'#BD10E0',
+									'#9013FE',
+									'#4A90E2',
+									'#50E3C2',
+									'#9B9B9B',
+								]}
+								styles={{
+									default: {
+										color: {
+											display: 'none',
+										},
+										saturation: {
+											paddingBottom: '40%',
+										},
+									},
+								}}
+							/>
+						)}
+					</VisuallyHiddenLoader>
 				</div>
 			</div>
 		);
