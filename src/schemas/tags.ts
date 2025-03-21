506e27b61be62e7766cadb2950c482afff102cbe
Pavel <pavelmalyv@gmail.com>
2025-03-21 15:59:49 +0300

    refactor(tags): убрать неиспользуемые поля из схемы

--- src/schemas/tags.ts
+++ src/schemas/tags.ts
@@ -1,13 +1,10 @@
-import { array, number, object, string } from 'yup';
+import { array, object, string } from 'yup';
 import { hexColorSchema, nameTagSchema } from './fields';
 
 export const tagSchema = object({
 	id: string().required(),
 	color: hexColorSchema,
 	name: string().required(),
-	createdAt: object({
-		seconds: number().required(),
-	}),
 });
 
 export const saveTagSchema = object({
