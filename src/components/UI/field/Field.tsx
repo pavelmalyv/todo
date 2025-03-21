25c7a497a986b017d0e2e7706386be671453a6d3
Pavel <pavelmalyv@gmail.com>
2025-03-21 15:59:49 +0300

    feat(Field): добавить скелетон

--- src/components/UI/field/Field.tsx
+++ src/components/UI/field/Field.tsx
@@ -1,9 +1,11 @@
 import classNames from 'classnames';
 import cl from './Field.module.scss';
 import FieldError from '../fieldError/FieldError';
+import ButtonIcon from '../buttonIcon/ButtonIcon';
+import Skeleton from 'react-loading-skeleton';
+import VisuallyHiddenLoader from '@/components/visuallyHiddenLoader/VisuallyHiddenLoader';
 
 import { forwardRef, useId, useState } from 'react';
-import ButtonIcon from '../buttonIcon/ButtonIcon';
 
 interface FieldProps {
 	label: string;
@@ -16,6 +18,8 @@ interface FieldProps {
 	errorMessage?: string;
 	autoComplete?: string;
 	isProtected?: boolean;
+	isLoading?: boolean;
+	isSkeleton?: boolean;
 	onChange?: React.ChangeEventHandler<HTMLInputElement>;
 	onBlur?: React.FocusEventHandler<HTMLInputElement>;
 	'aria-controls'?: string;
@@ -36,6 +40,8 @@ const Field = forwardRef<HTMLInputElement, FieldProps>(
 			errorMessage,
 			autoComplete,
 			isProtected,
+			isLoading = false,
+			isSkeleton = false,
 			onChange,
 			onBlur,
 			'aria-controls': ariaControls,
@@ -55,41 +61,49 @@ const Field = forwardRef<HTMLInputElement, FieldProps>(
 
 		return (
 			<div className={cl.wrapper}>
-				<label htmlFor={fieldId} className="visually-hidden">
-					{label}
-				</label>
-				<div className={cl.body}>
-					<input
-						ref={ref}
-						id={fieldId}
-						className={classNames(cl.field, { [cl['field_icon']]: isProtected })}
-						type={typeField}
-						name={name}
-						placeholder={placeholder}
-						value={value}
-						disabled={disabled}
-						maxLength={maxLength}
-						onChange={onChange}
-						onBlur={onBlur}
-						autoComplete={autoComplete}
-						aria-controls={ariaControls}
-						aria-invalid={ariaInvalid}
-						aria-describedby={errorMessageId}
-						aria-required={ariaRequired}
-					/>
-					{isProtected && (
-						<ButtonIcon
-							hiddenName={'Показать пароль'}
-							onClick={() => setIsProtectedState((prev) => !prev)}
-							aria-pressed={!isProtectedState}
-							aria-controls={fieldId}
-							className={cl.button}
-						>
-							{isProtectedState ? 'visibility' : 'visibility_off'}
-						</ButtonIcon>
+				<VisuallyHiddenLoader isLoading={isLoading}>
+					{isSkeleton ? (
+						<Skeleton className={cl.skeleton} />
+					) : (
+						<>
+							<label htmlFor={fieldId} className="visually-hidden">
+								{label}
+							</label>
+							<div className={cl.body}>
+								<input
+									ref={ref}
+									id={fieldId}
+									className={classNames(cl.field, { [cl['field_icon']]: isProtected })}
+									type={typeField}
+									name={name}
+									placeholder={placeholder}
+									value={value}
+									disabled={disabled}
+									maxLength={maxLength}
+									onChange={onChange}
+									onBlur={onBlur}
+									autoComplete={autoComplete}
+									aria-controls={ariaControls}
+									aria-invalid={ariaInvalid}
+									aria-describedby={errorMessageId}
+									aria-required={ariaRequired}
+								/>
+								{isProtected && (
+									<ButtonIcon
+										hiddenName={'Показать пароль'}
+										onClick={() => setIsProtectedState((prev) => !prev)}
+										aria-pressed={!isProtectedState}
+										aria-controls={fieldId}
+										className={cl.button}
+									>
+										{isProtectedState ? 'visibility' : 'visibility_off'}
+									</ButtonIcon>
+								)}
+							</div>
+							<FieldError id={errorMessageId} message={errorMessage ? errorMessage : null} />
+						</>
 					)}
-				</div>
-				<FieldError id={errorMessageId} message={errorMessage ? errorMessage : null} />
+				</VisuallyHiddenLoader>
 			</div>
 		);
 	},
