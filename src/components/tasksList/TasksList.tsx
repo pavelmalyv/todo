22a4bc2aea27e2c6a0560189154131968526fb23
Pavel <pavelmalyv@gmail.com>
2025-03-25 12:09:17 +0300

    feat(TasksList): интегрировать компонент TaskItem

--- src/components/tasksList/TasksList.tsx
+++ src/components/tasksList/TasksList.tsx
@@ -1,18 +1,10 @@
-import type { TaskId, Tasks } from '@/types/tasks';
+import type { Tasks } from '@/types/tasks';
 
 import cl from './TasksList.module.scss';
-import Checkbox from '../UI/checkbox/Checkbox';
 import ErrorMessage from '../UI/errorMessage/ErrorMessage';
-import Skeleton from 'react-loading-skeleton';
 import MessageInfo from '../UI/messageInfo/MessageInfo';
 import VisuallyHiddenLoader from '../visuallyHiddenLoader/VisuallyHiddenLoader';
-import ButtonIcon from '../UI/buttonIcon/ButtonIcon';
-import EditTaskModal from '../Modals/editTaskModal/EditTaskModal';
-
-import { auth } from '@/firebase';
-import { showError } from '@/utils/notification';
-import { setTaskDoc } from '@/utils/firestore';
-import { useState } from 'react';
+import TaskItem from '../taskItem/TaskItem';
 import { ERRORS_MESSAGES, LOADING_MESSAGES } from '@/consts/messages';
 
 type TasksListProps = {
@@ -24,25 +16,6 @@ type TasksListProps = {
 };
 
 const TasksList = ({ tasks, isLoading, notFoundMessage, error, children }: TasksListProps) => {
-	const [isOpenEditModals, setIsOpenEditModals] = useState<{ [key: TaskId]: boolean }>({});
-
-	const setIsOpenEditModal = (id: TaskId, value: boolean) => {
-		setIsOpenEditModals((prev) => ({ ...prev, [id]: value }));
-	};
-
-	const handleChange = async (e: React.ChangeEvent<HTMLInputElement>, id: TaskId) => {
-		const user = auth.currentUser;
-		if (!user) {
-			throw new Error('Invalid user value');
-		}
-
-		try {
-			await setTaskDoc(user.uid, id, { done: e.target.checked });
-		} catch (error) {
-			showError(ERRORS_MESSAGES.updateTask, error);
-		}
-	};
-
 	if (error) {
 		return <ErrorMessage message={ERRORS_MESSAGES.tasksLoading} error={error} />;
 	}
@@ -59,35 +32,7 @@ const TasksList = ({ tasks, isLoading, notFoundMessage, error, children }: Tasks
 
 							return (
 								<li className={cl.item} key={key}>
-									{task ? (
-										<>
-											<div className={cl.task}>
-												<Checkbox
-													style="through"
-													label={task.name}
-													checked={task.done}
-													onChange={(e) => handleChange(e, task.id)}
-												/>
-												<ButtonIcon
-													style="circle"
-													hiddenName="Редактировать задачу"
-													onClick={() => setIsOpenEditModal(task.id, true)}
-												>
-													edit
-												</ButtonIcon>
-											</div>
-
-											<EditTaskModal
-												initialData={task}
-												isOpen={isOpenEditModals[task.id] ?? false}
-												onClose={() => setIsOpenEditModal(task.id, false)}
-											/>
-										</>
-									) : (
-										<div className={cl.skeleton}>
-											<Skeleton height={22} />
-										</div>
-									)}
+									<TaskItem task={task} />
 								</li>
 							);
 						})}
