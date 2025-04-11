472ed5f96619a15813c8aa12e684abc711d63fce
Pavel <pavelmalyv@gmail.com>
2025-04-11 21:35:49 +0300

    fix(config): изменить длину заголовка тега

--- src/consts/config.ts
+++ src/consts/config.ts
@@ -5,7 +5,7 @@ export const TIMESTAMP_MAX = 99972349199000;
 export const PASSWORD_MAX_LENGTH = 4096;
 export const PASSWORD_MIN_LENGTH = 8;
 export const TASK_TITLE_MAX_LENGTH = 350;
-export const TAG_TITLE_MAX_LENGTH = 350;
+export const TAG_TITLE_MAX_LENGTH = 80;
 
 export const LIMIT_TASKS_DEFAULT = 30;
 export const LIMIT_UPCOMING_TASKS = 4;
