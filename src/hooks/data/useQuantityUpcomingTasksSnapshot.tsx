5933328d197e3d085b0f892133951e00daab35f2
Pavel <pavelmalyv@gmail.com>
2025-04-11 21:01:22 +0300

    refactor(useQuantityTasksSnapshot): перенести хук useQuantityTasksSnapshot в useQuantityUpcomingTasksSnapshot

--- src/hooks/data/useQuantityUpcomingTasksSnapshot.tsx
+++ src/hooks/data/useQuantityUpcomingTasksSnapshot.tsx
@@ -1,35 +1,55 @@
-import useQuantityTasksSnapshot from './useQuantityTasksSnapshot';
+import type { Tasks } from '@/types/tasks';
 
+import useTasksSnapshot from './useTasksSnapshot';
 import { getDateRanges } from '@/utils/date';
+import { useMemo } from 'react';
 import { LIMIT_UPCOMING_TASKS } from '@/consts/config';
+
+const getQuantityTasks = (tasks: Tasks) => {
+	let quantity = 0;
+
+	tasks.forEach((task) => {
+		if (!task.done) {
+			quantity++;
+		}
+	});
+
+	return quantity;
+};
 
 const useQuantityUpcomingTasksSnapshot = () => {
 	const dateRanges = getDateRanges();
 
-	const [today, isLoadingToday, errorToday] = useQuantityTasksSnapshot({
+	const [today, isLoadingToday, errorToday] = useTasksSnapshot({
 		timestampStart: dateRanges.today.start,
 		timestampEnd: dateRanges.today.end,
 		limit: LIMIT_UPCOMING_TASKS,
 	});
+	const todayQuantity = useMemo(() => (today ? getQuantityTasks(today) : null), [today]);
 
-	const [tomorrow, isLoadingTomorrow, errorTomorrow] = useQuantityTasksSnapshot({
+	const [tomorrow, isLoadingTomorrow, errorTomorrow] = useTasksSnapshot({
 		timestampStart: dateRanges.tomorrow.start,
 		timestampEnd: dateRanges.tomorrow.end,
 		limit: LIMIT_UPCOMING_TASKS,
 	});
+	const tomorrowQuantity = useMemo(
+		() => (tomorrow ? getQuantityTasks(tomorrow) : null),
+		[tomorrow],
+	);
 
-	const [near, isLoadingNear, errorNear] = useQuantityTasksSnapshot({
+	const [near, isLoadingNear, errorNear] = useTasksSnapshot({
 		timestampStart: dateRanges.near.start,
 		timestampEnd: dateRanges.near.end,
 		limit: LIMIT_UPCOMING_TASKS,
 	});
+	const nearQuantity = useMemo(() => (near ? getQuantityTasks(near) : null), [near]);
 
 	const isLoading = isLoadingToday || isLoadingTomorrow || isLoadingNear;
 	let quantity: number | null = null;
 	let error: Error | undefined;
 
-	if (today !== null && tomorrow !== null && near !== null) {
-		quantity = today + tomorrow + near;
+	if (todayQuantity !== null && tomorrowQuantity !== null && nearQuantity !== null) {
+		quantity = todayQuantity + tomorrowQuantity + nearQuantity;
 	}
 
 	if (errorToday || errorTomorrow || errorNear) {
